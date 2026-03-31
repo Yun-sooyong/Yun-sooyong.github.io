386878cb544d543396d77a551b3ba8a3bc7c225c
Yun-sooyong <wood5186@naver.com>
2026-04-01 00:21:13 +0900

    Adjust contact section alignment

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -28,17 +28,18 @@ export const defaultContentPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-      ],
-    }),
+    // Component.Flex({
+    //   components: [
+    //     {
+    //       Component: Component.Search(),
+    //       grow: true,
+    //     },
+    //     { Component: Component.Darkmode() },
+    //     { Component: Component.ReaderMode() },
+    //   ],
+    // }),
     Component.Explorer(),
+    Component.Spacer(),
     Component.ContactMe()
   ],
   right: [
